8bd05e5ca6ec6987174c9162d13ae5a4e5323e82
jooaodias <joaovitordias92040@gmail.com>
2022-11-03 22:23:20 -0300

    second e third day of nlw

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,6 +9,7 @@ import {
 import { THEME } from "./src/styles/theme";
 import { Loading } from "./src/components/Loading";
 import SignIn from "./src/screens/SignIn";
+import { AuthContextProvider } from "./src/context/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,13 +20,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {/* {!fontsLoaded ? <Loading /> : <SignIn />} */}
-      <SignIn />
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
