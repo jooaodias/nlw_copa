c5360af1b7ba2dadd117204ec606c65bbea7412f
jooaodias <joaovitordias92040@gmail.com>
2022-11-06 22:23:32 -0300

    started fifth day

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,9 +9,8 @@ import {
 import { THEME } from "./src/styles/theme";
 import { Loading } from "./src/components/Loading";
 import { AuthContextProvider } from "./src/context/AuthContext";
-import SignIn from "./src/screens/SignIn";
-import New from "./src/screens/New";
-import Pools from "./src/screens/Pools";
+
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +27,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,11 +1,44 @@
-import React from "react";
-import { VStack, Icon } from "native-base";
+import React, { useEffect, useState } from "react";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 
 import { Octicons } from "@expo/vector-icons";
+import { api } from "../services/api";
+import { PoolCard, PoolCardPros } from "../components/PoolCard";
+import { Loading } from "../components/Loading";
+import { EmptyPoolList } from "../components/EmptyPoolList";
 
 const Pools = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardPros[]>([]);
+
+  const { navigate } = useNavigation();
+  const toast = useToast();
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/pools");
+      setPools(response.data.pools);
+    } catch (error) {
+      console.log(error);
+
+      toast.show({
+        title: "Não foi possível carregar os boloões",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchPools();
+  }, []);
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Meus bolões" />
@@ -22,8 +55,20 @@ const Pools = () => {
             <Icon as={Octicons} name="search" color="black" size="md" />
           }
           title="BUSCAR BOLÃO POR CÓDIGO"
+          onPress={() => navigate("find")}
         />
       </VStack>
+
+      <FlatList
+        data={pools}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <PoolCard data={item} />}
+        px={5}
+        showsVerticalScrollIndicator={false}
+        _contentContainerStyle={{ pb: 10 }}
+        ListEmptyComponent={<EmptyPoolList />}
+      />
+      {/* <Loading /> */}
     </VStack>
   );
 };
